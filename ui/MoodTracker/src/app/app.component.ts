221b40110e4d3e0296ff54b9c67eb67f9091c322
Michael Keller <github@ike.ch>
2023-02-19 15:20:42 +1300

    Added Spring MVC skeleton.

--- ui/MoodTracker/src/app/app.component.ts
+++ ui/MoodTracker/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MoodTracker';
+  title = 'Mood Tracker';
+
+  // Object to save the response returned from the service.
+  myresponse: any;
+
+  // Url to fetch the employee records from the spring application.
+  readonly APP_URL = 'http://localhost:8082/MoodTracker';
+
+  constructor(private _http: HttpClient) { }
+
+  // Method to fetch all employees from the database table.
+  getAllEmployees() {
+    this._http.get(this.APP_URL + '/getemployees').subscribe(
+      data => {
+        this.myresponse = data;
+      },
+      error => {
+        console.log('Error occured', error);
+      }
+    );
+  }
 }
