75f2061d5aa021f08a1138417ee7c9910d1d2b10
Michael Keller <github@ike.ch>
2023-02-21 13:12:39 +1300

    Everything wired up!

--- db/scripts/create_db.sql
+++ db/scripts/create_db.sql
@@ -8,11 +8,11 @@ use moodtracker;
 CREATE USER 'service'@'%' IDENTIFIED BY 'secret';
 GRANT INSERT, UPDATE, DELETE, SELECT on moodtracker.* TO 'service'@'%' WITH GRANT OPTION;
 
-CREATE TABLE `Mood` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE `mood` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
   `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `personal_token` varchar(36) NOT NULL DEFAULT '',
-  `mood_value` int(1) NOT NULL DEFAULT 1,
+  `mood_value` int NOT NULL DEFAULT '1',
   `comment` varchar(350) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb3;
